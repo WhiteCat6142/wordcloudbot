ae2b85eb668ec5c183e56eefa322103ae51d045e
WhiteCat6142 <whitecat6142+git@gmail.com>
2024-07-07 02:42:35 +0900

    Dockerfile modified-2

--- entry.sh
+++ entry.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-mkdir /home/bun/.oci
-cp /env/config /home/bun/.oci/config
-/root/bin/oci setup repair-file-permissions --file /home/bun/.oci/config
+mkdir /root/.oci
+cp /env/config /root/.oci/config
+/root/bin/oci setup repair-file-permissions --file /root/.oci/config
 cp /env/prikey.pem /home/bun/prikey.pem
 /root/bin/oci setup repair-file-permissions --file /home/bun/prikey.pem
 cp /env/.env /home/bun/app/wordcloudbot/.env
