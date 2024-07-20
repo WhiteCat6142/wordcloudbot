f1162bb8f5e04814b23d92a9766abe9191ce93ec
WhiteCat6142 <whitecat6142+git@gmail.com>
2024-07-20 21:09:58 +0900

    update reaction for npub

--- tool.ts
+++ tool.ts
@@ -1,11 +1,13 @@
 import { getPublicKey } from './nostr-tools/pure'
 import { bytesToHex, hexToBytes } from '@noble/hashes/utils'
+import * as nip19 from 'nostr-tools/nip19'
 import { useWebSocketImplementation, Relay } from './nostr-tools/relay'
 import WebSocket from 'ws'
 useWebSocketImplementation(WebSocket)
 
 const sk = hexToBytes(process.env.prikey)
 const pk = getPublicKey(sk)
+const npub = nip19.npubEncode(pk)
 
 const now = ()=>{return Math.floor(Date.now() / 1000)}
 
@@ -17,13 +19,14 @@ function push(t: number): void{
       proc.unref();
 }
 
-let last = 0
+let last = now()
 const req1 = {kinds: [1], "#p":[pk], since:now()}
 const sub = relay.subscribe([req1], {
   onevent(ev) {
     console.log('we got the event:', ev)
     const t = ev["created_at"]
-    if (t - last >= 300){
+    const c = ev["content"]
+    if (((t - last) > 360)&&(c.includes("nostr:"+npub))){
       push(t).then()
       last=t
     }
